64a9a3bf8e150be6814805742e23dafde3283286
Aman Raj <archanaamanraj@gmail.com>
2022-02-22 02:50:31 +0530

    backend updated

--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,103 @@
-require('dotenv').config()
-const express = require("express")
-const cors = require('cors');
-// const nearAPI = require("near-api-js");
-const { nanoid } = require('nanoid');
-const { ObjectId } = require('mongodb');
-const { genRtcTokenUser } = require('./utils/agora.js');
-const { connectDb, client } = require("./utils/db.js");
-// Middlewares
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.get("/", async function (_req, res) {
-  res.send("Meta Backend API");
+const path = require("path");
+require("dotenv").config({
+  path: path.resolve(process.cwd(), ".env"),
 });
 
-app.use('/add', async (req, res) => {
-  const { a, b } = req.body;
-  const rooms = client.db("meta").collection("rooms");
-  const nonce = `I am signing this nonce: ${nanoid()}`;
-  const result = await rooms.updateOne(
-    { address: address },
-    { $set: { nonce: nonce, expires: Date.now() + 604800000 } }
-  );
-  console.log(result);
-  res.status(200).send(result);
-})
+const express = require("express");
+const app = express();
+const http = require("http");
+const { connectDb, client } = require("./db");
+const server = http.createServer(app);
 
-app.use('/getToken', async (req, res) => {
-  // const rooms = client.db("meta").collection("rooms");
-  // const result = await rooms.findOne({ _id: ObjectId("61f3eb7ff128c60c5e0afd78") });
-  // console.log(result);
-  const result = await genRtcTokenUser('hehe', 1, 'subscriber');
-  res.status(200).send(result);
-})
+const io = require("socket.io")(server, {
+  origin: "*",
+  cors: true,
+});
+const signalServer = require("simple-signal-server")(io);
+const rooms = {};
+const socketRooms = {};
 
-// DB connect and listen to server
+io.on("connection", (socket) => {
+  socket.on("joinRoom", async (payload) => {
+    const data = JSON.parse(payload);
+    const { id } = data;
+    socket.join(data.id);
+    console.log(socketRooms);
+    if (!socketRooms[id]) {
+      socketRooms[id] = new Set();
+    }
+    socketRooms[id].add(data.user);
+    io.in(id).emit("users", JSON.stringify(Array.from(socketRooms[id])));
+    socket.on("disconnect", () => {
+      io.in(id).emit("delete", data.user.streamId);
+      socketRooms[id].delete(data.user);
+      io.in(id).emit("users", JSON.stringify(Array.from(socketRooms[id])));
+    });
+    socket.on("promote", (promoteId) => {
+      socketRooms[id] = new Set(
+        Array.from(socketRooms[id]).map((mem) => {
+          if (mem.streamId === promoteId) {
+            return { ...mem, speaker: true };
+          }
+          return mem;
+        })
+      );
+
+      io.in(id).emit("users", JSON.stringify(Array.from(socketRooms[id])));
+      io.in(id).emit("lift", promoteId);
+    });
+    socket.on("speaking", (speakId) => {
+      Array.from(socketRooms[id]).find(
+        (s) => s.streamId === speakId && s.speaker
+      ) && io.in(id).emit("spoke", speakId);
+    });
+    const collection = client.db(process.env.MONGO_DB).collection("rooms");
+
+    async function* genUpdates(collection) {
+      const changeStream = collection.watch();
+      while (true) {
+        const next = await changeStream.next();
+        yield next;
+      }
+    }
+    for await (const { operationType, updateDescription } of genUpdates(
+      collection
+    )) {
+      operationType === "update" &&
+        io.in(id).emit("update", updateDescription.updatedFields);
+    }
+  });
+});
+
+signalServer.on("discover", (request) => {
+  console.log("in signal server")
+  const roomID = request.discoveryData;
+  signalServer.on("disconnect", (socket) => {
+    const clientID = socket.id;
+    rooms[roomID].delete(clientID);
+  });
+  if (!rooms[roomID]) {
+    rooms[roomID] = new Set();
+  }
+  request.discover({
+    roomResponse: roomID,
+    peers: Array.from(rooms[roomID]),
+  });
+
+  if (request.socket.roomID !== roomID) {
+    request.socket.roomID = roomID;
+    rooms[roomID].add(request.socket.id);
+  }
+});
+
 connectDb()
   .then(() => {
-    app.listen(process.env.PORT || 80, () => {
-      console.log(`Server starting on port ${process.env.PORT || 80}`)
-    });
+    console.log("db connected");
+    server.listen(80, (err) =>
+      console.log(err || "server listening on 80")
+    );
   })
   .catch((e) => {
-    console.log(e);
+    console.log(e.message);
     process.exit(1);
   });
